4072df0e7bf894a0c5d1afd5bcadc73321e6f46c
Adam Hannigan <adamhannigan81@gmail.com>
2025-01-13 10:15:11 +1100

    [FEAT] Reputation

--- src/lib/flags.ts
+++ src/lib/flags.ts
@@ -80,6 +80,7 @@ const featureFlags = {
   WEATHER_SHOP: testnetFeatureFlag,
   FRUIT_PATCH_QUICK_SELECT: defaultFeatureFlag,
   SEASONAL_EVENTS_NOTIFICATIONS: testnetFeatureFlag,
+  REPUTATION: defaultFeatureFlag,
 } satisfies Record<string, FeatureFlag>;
 
 export type FeatureName = keyof typeof featureFlags;
